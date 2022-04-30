3c02aec0521acf93a33c780022b847a85d90cc30
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2022-04-30 02:07:02 +0200

    Added tests for (strings, arrays)

--- tests/Rules/MinTest.php
+++ tests/Rules/MinTest.php
@@ -5,10 +5,26 @@ use Validator\Rules\MinRule;
 
 Validator::registerRule(MinRule::class);
 
-test('Min: valid case', function () {
+test('Min: valid case (number)', function () {
     expect(Validator::init(6)->min(5)->validate())->toBe(true);
 });
 
-test('Min: invalid case', function () {
+test('Min: valid case (array)', function () {
+    expect(Validator::init([1])->min(1)->validate())->toBe(true);
+});
+
+test('Min: valid case (string)', function () {
+    expect(Validator::init('test')->min(4)->validate())->toBe(true);
+});
+
+test('Min: invalid case (number)', function () {
     expect(Validator::init(5)->min(6)->validate())->toBe(false);
+});
+
+test('Min: invalid case (array)', function () {
+    expect(Validator::init([])->min(1)->validate())->toBe(false);
+});
+
+test('Min: invalid case (string)', function () {
+    expect(Validator::init('test')->min(5)->validate())->toBe(false);
 });
