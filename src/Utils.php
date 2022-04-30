e20467299f1daef6ee3ea93cf55de0e3f84d2665
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2022-04-30 19:31:48 +0200

    Added PHPDoc for `Utils::canBeString`

--- src/Utils.php
+++ src/Utils.php
@@ -6,6 +6,12 @@ use Stringable;
 
 class Utils
 {
+    /**
+     * Check if value can be casted to string.
+     *
+     * @param mixed $value
+     * @return bool
+     */
     public static function canBeString(mixed $value): bool
     {
         return $value === null || is_scalar($value) || $value instanceof Stringable;
