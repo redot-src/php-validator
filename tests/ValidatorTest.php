4feb8feba1ac6c40b9d8e98c8115ac532686409f
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2022-11-28 12:15:27 +0200

    Update tests

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -5,16 +5,26 @@ use Redot\Validator\AbstractRule;
 use Redot\Validator\Errors\InvalidRuleException;
 use Redot\Validator\Errors\RuleNotFoundException;
 use Redot\Validator\Errors\DuplicateRuleException;
+use Redot\Validator\Rules\MinRule;
 
-class ValidatorTest extends AbstractRule
+class EqualsOne extends AbstractRule
 {
+    /**
+     * @inheritDoc
+     */
     protected string $message = 'Value should be equal to 1.';
 
+    /**
+     * @inheritDoc
+     */
     public function getName(): string
     {
         return 'equalsOne';
     }
 
+    /**
+     * @inheritDoc
+     */
     public function validate(mixed $value, mixed ...$params): bool
     {
         return $value === 1;
@@ -22,8 +32,8 @@ class ValidatorTest extends AbstractRule
 }
 
 it('can register a rule', function () {
-    Validator::addRule(ValidatorTest::class);
-    expect(Validator::hasRule(ValidatorTest::class))->toBe(true);
+    Validator::addRule(EqualsOne::class);
+    expect(Validator::hasRule(EqualsOne::class))->toBe(true);
 });
 
 it('can validate a rule', function () {
@@ -37,7 +47,7 @@ it('throws an exception if rule is not an instance of Rule', function () {
 });
 
 it('throws an exception if rule is already registered', function () {
-    expect(fn () => Validator::addRule(ValidatorTest::class))
+    expect(fn () => Validator::addRule(EqualsOne::class))
         ->toThrow(DuplicateRuleException::class);
 });
 
@@ -58,13 +68,21 @@ it('can validate multiple entries', function () {
 });
 
 it('can change the default error message', function () {
-    Validator::setMessages([ValidatorTest::class => 'test']);
+    Validator::setMessages([EqualsOne::class => 'test']);
     expect(Validator::init(2)->equalsOne()->getErrors())
         ->toBe(['equalsOne' => 'test']);
 
     Validator::setMessages(['equalsOne' => 'test2']);
     expect(Validator::init(2)->equalsOne()->getErrors())
         ->toBe(['equalsOne' => 'test2']);
+});
+
+it('can read parameters to error message', function () {
+    $minimum = 5;
+    $message = (new MinRule)->getMessage();
+
+    expect(Validator::init('test')->min($minimum)->getErrors())
+        ->toBe(['min' => str_replace('{0}', $minimum, $message)]);
 });
 
 it('can validate a rule statically', function () {
